ccf418236063e4b497516aa2bb016344bd17b148
Efterklang <gaojiaxing2004@qq.com>
2025-12-08 17:52:10 +0800

    优化插件加载逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import type { StoreFunction, StoreFunctionData } from "hexo/dist/extend/renderer";
 import { MarkdownRenderer } from "./renderer";
 
-const rendererInstance = new MarkdownRenderer(hexo);
+let rendererInstance: MarkdownRenderer | null = null;
 
+function getRenderer(): MarkdownRenderer {
+	if (!rendererInstance) {
+		rendererInstance = new MarkdownRenderer(hexo);
+	}
+	return rendererInstance;
+}
+
+// Hexo在执行`clean`命令删除`public`目录时也会加载各个插件，包括本插件中渲染器的注册
+// 所以把渲染器的实例化延迟到真正需要渲染时再进行，避免在`clean`命令时进行不必要的初始化操作
 const render: StoreFunction = async (data: StoreFunctionData): Promise<string> =>
-	await rendererInstance.render(data);
+	await getRenderer().render(data);
 
 render.disableNunjucks = Boolean(hexo.config.markdown_exit.disableNunjucks);
 
